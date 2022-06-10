a6de9afcb9d601ccb6bea10fd32380211bf56063
NicolaRaulPetrarca <106910581+NicolaRaulPetrarca@users.noreply.github.com>
2022-06-10 11:29:45 +0200

    Update calcolatrice.js

--- calcolatrice.js
+++ calcolatrice.js
@@ -7,20 +7,3 @@ function calcola(addendo1,addendo2,operazione){
 function stampa(x){
         document.getElementById("addendo").innerHTML = x;
 }
-
-function somma(addendo1,addendo2){
-    document.getElementById("risultato").innerHTML = (parseInt(addendo1)+parseInt(addendo2));
-}
-
-function sottrazione(addendo1, addendo2){
-    document.getElementById("risultato").innerHTML = (parseInt(addendo1)-parseInt(addendo2));  
-}
-
-function moltiplicazione(addendo1, addendo2){
-    document.getElementById("risultato").innerHTML = (parseInt(addendo1)*parseInt(addendo2)); 
-}
-
-function divisione(addendo1, addendo2){
-    document.getElementById("risultato").innerHTML = (parseInt(addendo1)/parseInt(addendo2));
-}
-
